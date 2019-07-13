8fd6ba4af6c48242126d853be2bea256f958ebde
Markus Johansson <markus@obviuse.se>
2019-07-13 16:34:25 +0200

    ng switch and passing functions

---
+++ AngularJSPlayProject/Scripts/App/Controllers/index.controller.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    function Controller($http) {
+
+        var vm = this;
+        vm.parentLabel = "My Label";
+        vm.parentInfo = "Some information goes here";
+        vm.parentDetails = "Some parent details here";
+
+        vm.directiveUpdated = function (args) {
+            console.log('Diretive updated:', args);
+            alert('Directive was updated');
+        };
+    }
+
+    angular.module("ngDemo").controller("indexController", ['$http', Controller]);
+})();
---
+++ AngularJSPlayProject/Scripts/App/Directives/demoDirective/demo.directive.js
@@ -0,0 +1,25 @@
+﻿
+(function () {
+    "use strict";
+
+    function demoDirective($http) {
+
+        return {
+            replace:true, // This will replace the <multi-choice/> element from the calling view with the content of the directive.
+            scope: {
+                label: '@label', //@ performs "attribute"-binding. If the parent scope changes it'll be reflected, not the other way around. We can still change the local scope but this will be overwritten when/if the parent scope changes.
+                info: '@', //if the isolated scope property name is the same as the passed we can just use the @-sign. In this case this would be the same as @info
+                moreInfo: '@moreInfo',
+                details : '=details',
+                items: '=items', //= performs a "two-way" binding, this directive can change it and it will reflect outside changes
+                onUpdate : '&onUpdate'
+            },
+            templateUrl: '/scripts/app/directives/demoDirective/demo.html'
+            //template: 'Hallo World'
+        };
+    }
+
+    angular.module("ngDemo").directive("demoDirective", ['$http', demoDirective]);
+
+})();
+
